e1cd29ee7305c6eabc942066d4f7513a3217e690
James Shubin <james@shubin.ca>
2015-01-23 18:01:46 -0500

    Add changes to make it explicitly easier to build Fedora 20 images.
    This used to be in a feature branch that wasn't recently refreshed.

---
+++ centos-7.0/versions/fedora-20.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# to use this script, from its parent dir, run: ./versions/<script>.sh <target>
+# you'll want to edit the below bash variables to match your use cases :)
+# eg: ./versions/fedora-20.sh upload
+# to make your own base image and upload it to your own server somewhere.
+
+VERSION='fedora-20'		# pick from the output of virt-builder -l
+POSTFIX=''
+SIZE='40'			# disk size of image
+SERVER=''	# connect over ssh (add your public key first)
+REMOTE_PATH=''	# make a $VERSION directory in this dir
+KEYS='puppetlabs'
+REPOS='f20-puppet'
+IMAGES=''
+PACKAGES='vim-enhanced git wget file man tree nmap tcpdump htop lsof telnet mlocate bind-utils koan iftop yum-utils nmap-ncat psmisc bash-completion moreutils'	# list of extra packages to include
+make VERSION=$VERSION POSTFIX=$POSTFIX SIZE=$SIZE SERVER=$SERVER REMOTE_PATH=$REMOTE_PATH KEYS="$KEYS" REPOS="$REPOS" IMAGES="$IMAGES" PACKAGES="$PACKAGES" $@
+
