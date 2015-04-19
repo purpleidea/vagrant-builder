4015cddfdd751cb2449cd97c3e07d8522047795c
James Shubin <james@shubin.ca>
2015-04-18 20:28:25 -0400

    Add CentOS 7.1 versions
    Thanks to rwmjones for rolling the .xz base images.
    Note that the -docker version now includes kubernetes and flannel by
    default. If you want a cleaner image, use the non-docker version or make
    your own versions file! That's the whole point :)

---
+++ v7/versions/centos-7.1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# to use this script, from its parent dir, run: ./versions/<script>.sh <target>
+# you'll want to edit the below bash variables to match your use cases :)
+# eg: ./versions/centos-7.0.sh upload
+# to make your own base image and upload it to your own server somewhere.
+
+VERSION='centos-7.1'		# pick from the output of virt-builder -l
+POSTFIX=''
+SIZE='40'			# disk size of image
+SERVER=''			# connect over ssh (add your public key first)
+REMOTE_PATH=''			# make a $VERSION directory in this dir
+KEYS='EPEL-7 puppetlabs'	# add extra keys to the base image
+REPOS='epel7 epel7-testing el7-puppet'	# add extra repos to the base image
+DOCKER=''			# list of docker images to include
+PACKAGES='sudo vim-enhanced git wget file man tree nmap tcpdump htop lsof telnet mlocate bind-utils koan iftop yum-utils nc psmisc bash-completion moreutils'	# list of extra packages to include
+make VERSION=$VERSION POSTFIX=$POSTFIX SIZE=$SIZE SERVER=$SERVER REMOTE_PATH=$REMOTE_PATH KEYS="$KEYS" REPOS="$REPOS" DOCKER="$DOCKER" PACKAGES="$PACKAGES" $@
+
