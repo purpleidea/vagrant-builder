273b2308d97c37f1ebfdfe71fab248a80a90ce01
Randy Barlow <rbarlow@redhat.com>
2015-06-04 18:14:05 -0400

    Allow sudo -u for the vagrant user.

--- v7/files/user.sh
+++ v7/files/user.sh
@@ -10,7 +10,5 @@ fi
 grep '^admin:' /etc/group > /dev/null || groupadd admin
 usermod -G admin vagrant
 
-#echo 'Defaults    env_keep += "SSH_AUTH_SOCK"' >> /etc/sudoers
-echo '%admin ALL=NOPASSWD: ALL' >> /etc/sudoers
+echo '%admin ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 sed -i 's/Defaults\s*requiretty/Defaults !requiretty/' /etc/sudoers
-
