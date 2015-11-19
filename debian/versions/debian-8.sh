9f69bc6af7ae361321262680a2f036061a47349c
James Shubin <james@shubin.ca>
2015-11-19 01:46:22 -0500

    Add the ability to produce up-to-date debian images

--- debian/versions/debian-8.sh
+++ debian/versions/debian-8.sh
@@ -2,16 +2,16 @@
 
 # to use this script, from its parent dir, run: ./versions/<script>.sh <target>
 # you'll want to edit the below bash variables to match your use cases :)
-# eg: ./versions/centos-7.0.sh upload
+# eg: ./versions/debian-8.sh upload
 # to make your own base image and upload it to your own server somewhere.
 
 VERSION='debian-8'		# pick from the output of virt-builder -l
 POSTFIX=''			# add this string onto the base version
 SIZE='40'			# disk size of image
-SERVER='user@host.example.org'	# connect over ssh (add your public key first)
-REMOTE_PATH='public_html/vagrant'	# make a $VERSION directory in this dir
-KEYS=''	# add extra keys to the base image
-REPOS=''	# add extra repos to the base image
+SERVER=''			# connect over ssh (add your public key first)
+REMOTE_PATH=''			# make a $VERSION directory in this dir
+KEYS=''				# add extra keys to the base image
+REPOS=''			# add extra repos to the base image
 DOCKER=''			# list of docker images to include
-PACKAGES='sudo vim-nox git wget file man tree nmap tcpdump htop lsof telnet mlocate bind-utils iftop nc psmisc bash-completion moreutils'	# list of extra packages to include
+PACKAGES='sudo vim-nox git wget file man tree nmap tcpdump htop lsof telnet mlocate bind9-host iftop netcat psmisc bash-completion moreutils'	# list of extra packages to include
 make VERSION=$VERSION POSTFIX=$POSTFIX SIZE=$SIZE SERVER=$SERVER REMOTE_PATH=$REMOTE_PATH KEYS="$KEYS" REPOS="$REPOS" DOCKER="$DOCKER" PACKAGES="$PACKAGES" $@
