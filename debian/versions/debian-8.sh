a7ae11654b2c77cbc37c7bdc1e0525ef740de2fd
James Shubin <james@shubin.ca>
2016-02-22 19:55:54 -0500

    Add GNU make to package dependencies

--- debian/versions/debian-8.sh
+++ debian/versions/debian-8.sh
@@ -13,5 +13,5 @@ REMOTE_PATH=''			# make a $VERSION directory in this dir
 KEYS=''				# add extra keys to the base image
 REPOS=''			# add extra repos to the base image
 DOCKER=''			# list of docker images to include
-PACKAGES='sudo vim-nox git wget file man tree nmap tcpdump htop lsof telnet mlocate bind9-host iftop netcat psmisc bash-completion moreutils'	# list of extra packages to include
+PACKAGES='sudo make vim-nox git wget file man tree nmap tcpdump htop lsof telnet mlocate bind9-host iftop netcat psmisc bash-completion moreutils'	# list of extra packages to include
 make VERSION=$VERSION POSTFIX=$POSTFIX SIZE=$SIZE SERVER=$SERVER REMOTE_PATH=$REMOTE_PATH KEYS="$KEYS" REPOS="$REPOS" DOCKER="$DOCKER" PACKAGES="$PACKAGES" $@
